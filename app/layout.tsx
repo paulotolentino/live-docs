0617918e53ceb5303fce23fa5ea5193d91dd558a
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2024-07-27 01:15:46 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { Inter as FontSans } from "next/font/google"
+import "./globals.css"
+
+import { cn } from "@/lib/utils"
+import { Metadata } from "next"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "Your go-to collaborative editor",
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
